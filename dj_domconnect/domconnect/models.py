fca647e1cbcd32e69db89dbcd68776b88710d931
Alexey <alexey-sin@yandex.ru>
2022-01-24 12:11:28 +0300

    house

--- dj_domconnect/domconnect/models.py
+++ dj_domconnect/domconnect/models.py
@@ -10,13 +10,13 @@ class DomconnectCrmLid(models.Model):
     title = models.CharField(
         max_length = 255,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'Титл',
     )
     status_id = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name='Статус',
     )
     create_date = models.DateTimeField(
@@ -32,110 +32,110 @@ class DomconnectCrmLid(models.Model):
     source_id = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name='Источник',
     )
     assigned_by_id = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name='ASSIGNED_BY_ID',
     )
     crm_1493416385 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'Сумма тарифа',
     )
     crm_1499437861 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'ИНН/Организация',
     )
     crm_1580454770 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'Звонок?',
     )
     crm_1534919765 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'Группы источников',
     )
     crm_1571987728429 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'Провайдеры ДК',
     )
     crm_1592566018 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'ТИп лида',
     )
     crm_1493413514 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'Провайдер',
     )
     crm_1492017494 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'Область',
     )
     crm_1492017736 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'Город',
     )
     crm_1498756113 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'Юр. лицо',
     )
 
     crm_1615982450 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'utm_source',
     )
     crm_1615982567 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'utm_medium',
     )
     crm_1615982644 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'utm_campaign',
     )
     crm_1615982716 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'utm_term',
     )
     crm_1615982795 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
         verbose_name = 'utm_content',
     )
     crm_1640267556 = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = ' ',
         verbose_name = 'utm_group',
     )
 
@@ -151,11 +151,11 @@ class GlobalVariable(models.Model):
     val_str = models.CharField(
         max_length = 100,
         blank = True,
-        null = True,
+        default = '',
     )
     val_bool = models.BooleanField(
         blank = True,
-        null = True,
+        default = 'False',
     )
     val_int = models.IntegerField(
         blank = True,
@@ -173,7 +173,7 @@ class GlobalVariable(models.Model):
     )
     descriptions = models.TextField(
         blank = True,
-        null = True,
+        default = '',
         verbose_name='Описание',
     )
 
--- dj_domconnect/domconnect/views.py
+++ dj_domconnect/domconnect/views.py
@@ -44,9 +44,6 @@ def downloadLidsFromCRM(request):
         from_modify = from_modify - timedelta(seconds=1)
         str_from_modify = from_modify.strftime('%Y-%m-%dT%H:%M:%S')
 
-
-    # return JsonResponse({'is_run': False})
-
     # Проверим идет ли загрузка
     is_run = False
     for thread in threading.enumerate():
@@ -65,25 +62,25 @@ def downloadLidsFromCRM(request):
     if is_stop:
         gvar_go, _ = GlobalVariable.objects.get_or_create(key='go_download_crm')
         gvar_go.val_bool = False
-        gvar_go.save()
+        gvar_go.save(update_fields=['val_bool'])
 
     is_start = request.GET.get('start')
     if is_start and not is_run:
         # Разрешим загрузку в глобальной переменной
         gvar_go, _ = GlobalVariable.objects.get_or_create(key='go_download_crm')
         gvar_go.val_bool = True
-        gvar_go.save()
+        gvar_go.save(update_fields=['val_bool'])
 
         # Обнулим глоб. переменную текущей позиции
         gvar_cur, _ = GlobalVariable.objects.get_or_create(key='cur_num_download_crm')
         gvar_cur.val_int = 0
-        gvar_cur.save()
+        gvar_cur.save(update_fields=['val_int'])
 
         # Очистим лог
-        gvar, _ = GlobalVariable.objects.get_or_create(key='log_download')
-        gvar.val_datetime = None
-        gvar.descriptions = None
-        gvar.save()
+        gvar_log, _ = GlobalVariable.objects.get_or_create(key='log_download_crm')
+        gvar_log.val_datetime = None
+        gvar_log.descriptions = ''
+        gvar_log.save(update_fields=['val_datetime', 'descriptions'])
 
         # Запустим поток загрузки
         th = Thread(target=thread_download_crm, name=thread_name, args=(str_from_modify, ))
@@ -93,14 +90,18 @@ def downloadLidsFromCRM(request):
 
 
 def fix_result_download_crm(mess):
-    gvar, _ = GlobalVariable.objects.get_or_create(key='log_download_crm')
-    gvar.val_datetime = datetime.now()
-    gvar.descriptions += mess
-    gvar.save()
+    gvar_log, _ = GlobalVariable.objects.get_or_create(key='log_download_crm')
+    gvar_log.val_datetime = datetime.now()
+    old_str = str(gvar_log.descriptions)
+    if old_str: gvar_log.descriptions = f'{old_str}\n{mess}'
+    else: gvar_log.descriptions = mess
+    gvar_log.save(update_fields=['val_datetime', 'descriptions'])
 
 def append_lids(lids):
     cnt_err = 0
+    cnt_ok = 0
     for new_lid in lids:
+        # print(new_lid)
         err = True
         try:
             lid, _ = DomconnectCrmLid.objects.get_or_create(id_lid=new_lid.get('ID'))
@@ -108,48 +109,59 @@ def append_lids(lids):
             lid.status_id = new_lid.get('STATUS_ID')
             lid.create_date = datetime.strptime(new_lid.get('DATE_CREATE')[:-6], '%Y-%m-%dT%H:%M:%S')  # "2022-01-01T04:43:22+03:00"
             lid.modify_date = datetime.strptime(new_lid.get('DATE_MODIFY')[:-6], '%Y-%m-%dT%H:%M:%S')
-            lid.assigned_by_id = new_lid.get('ASSIGNED_BY_ID')
-            lid.crm_1493416385 = new_lid.get('UF_CRM_1493416385')
-            lid.crm_1499437861 = new_lid.get('UF_CRM_1499437861')
-            lid.crm_1580454770 = new_lid.get('UF_CRM_1580454770')
+            val_field = new_lid.get('ASSIGNED_BY_ID')
+            if val_field: lid.assigned_by_id = val_field
+            val_field = new_lid.get('UF_CRM_1493416385')
+            if val_field: lid.crm_1493416385 = val_field
+            val_field = new_lid.get('UF_CRM_1499437861')
+            if val_field: lid.crm_1499437861 = val_field
+            val_field = new_lid.get('UF_CRM_1580454770')
+            if val_field: lid.crm_1580454770 = val_field
             val_field = new_lid.get('UF_CRM_1534919765')
             if val_field and len(val_field) > 0: lid.crm_1534919765 = val_field[0]
-            lid.crm_1571987728429 = new_lid.get('UF_CRM_1571987728429')
+            val_field = new_lid.get('UF_CRM_1571987728429')
+            if val_field: lid.crm_1571987728429 = val_field
             val_field = new_lid.get('UF_CRM_1592566018')
             if val_field and len(val_field) > 0: lid.crm_1592566018 = val_field[0]
-            lid.crm_1493413514 = new_lid.get('UF_CRM_1493413514')
-            lid.crm_1492017494 = new_lid.get('UF_CRM_1492017494')
-            lid.crm_1492017736 = new_lid.get('UF_CRM_1492017736')
-            lid.crm_1498756113 = new_lid.get('UF_CRM_1498756113')
-            lid.crm_1615982450 = new_lid.get('UF_CRM_1615982450')
-            lid.crm_1615982567 = new_lid.get('UF_CRM_1615982567')
-            lid.crm_1615982644 = new_lid.get('UF_CRM_1615982644')
-            lid.crm_1615982716 = new_lid.get('UF_CRM_1615982716')
-            lid.crm_1615982795 = new_lid.get('UF_CRM_1615982795')
-            lid.crm_1640267556 = new_lid.get('UF_CRM_1640267556')
+            val_field = new_lid.get('UF_CRM_1493413514')
+            if val_field: lid.crm_1493413514 = val_field
+            val_field = new_lid.get('UF_CRM_1492017494')
+            if val_field: lid.crm_1492017494 = val_field
+            val_field = new_lid.get('UF_CRM_1492017736')
+            if val_field: lid.crm_1492017736 = val_field
+            val_field = new_lid.get('UF_CRM_1498756113')
+            if val_field: lid.crm_1498756113 = val_field
+            val_field = new_lid.get('UF_CRM_1615982450')
+            if val_field: lid.crm_1615982450 = val_field
+            val_field = new_lid.get('UF_CRM_1615982567')
+            if val_field: lid.crm_1615982567 = val_field
+            val_field = new_lid.get('UF_CRM_1615982644')
+            if val_field: lid.crm_1615982644 = val_field
+            val_field = new_lid.get('UF_CRM_1615982716')
+            if val_field: lid.crm_1615982716 = val_field
+            val_field = new_lid.get('UF_CRM_1615982795')
+            if val_field: lid.crm_1615982795 = val_field
+            val_field = new_lid.get('UF_CRM_1640267556')
+            if val_field: lid.crm_1640267556 = val_field
             lid.save()
             err = False
+            cnt_ok += 1
         except Exception as e: 
             print(str(e))
         if err: cnt_err += 1
-    return cnt_err
+    return cnt_ok, cnt_err
 
 
 def thread_download_crm(str_from_modify):
-    gvar, create = GlobalVariable.objects.get_or_create(key='url_download_crm')
+    gvar_url, create = GlobalVariable.objects.get_or_create(key='url_download_crm')
     url = 'https://crm.domconnect.ru/rest/371/ao3ct8et7i7viajs/crm.lead.list'
     if create:
-        gvar.val_str = url
-        gvar.save()
+        gvar_url.val_str = url
+        gvar_url.save()
     else:
-        url = gvar.val_str
+        url = gvar_url.val_str
     
     print(f'start thread {str_from_modify}')
-
-    # dt_start = dt.strptime(from_date, '%d.%m.%Y')
-    # str_dt_start = dt_start.strftime('%Y-%m-%dT%H:%M:%S')
-    go_next = 0
-    go_total = 0
 
     headers = {
         'Content-Type': 'application/json',
@@ -157,7 +169,10 @@ def thread_download_crm(str_from_modify):
         'User-Agent': 'Apache-HttpClient/4.1.1 (java 1.5)',
     }
 
+    go_next = 0
+    go_total = 0
     cnt_err = 0
+    cnt_ok = 0
     while True:
         gvar_go, _ = GlobalVariable.objects.get_or_create(key='go_download_crm')
         if gvar_go.val_bool == False: break
@@ -205,7 +220,8 @@ def thread_download_crm(str_from_modify):
                 go_next = answer.get('next')
                 go_total = answer.get('total')
                 print(go_next, go_total)
-                cnt_err += append_lids(result)
+                ok, err = append_lids(result)
+                cnt_ok += ok; cnt_err += err
                 if not go_next: break
             else: return fix_result_download_crm(f'Ошибка get_lids: responce.status_code: {responce.status_code}\n{responce.text}')
         except Exception as e: fix_result_download_crm(f'Ошибка get_lids: try: requests.post {e}')
@@ -215,12 +231,12 @@ def thread_download_crm(str_from_modify):
         gvar_tot, _ = GlobalVariable.objects.get_or_create(key='tot_num_download_crm')
         gvar_tot.val_int = go_total
         gvar_tot.save()
-        time.sleep(1)
+        time.sleep(2)
     
     gvar_go, _ = GlobalVariable.objects.get_or_create(key='go_download_crm')
     gvar_go.val_bool = False
     gvar_go.save()
-    mess = f'Обработано лидов: {go_total}. Ошибок: {cnt_err}'
+    mess = f'Обработано лидов: {cnt_ok}. Ошибок: {cnt_err}'
     fix_result_download_crm(mess)
     print('stop thread')
 
